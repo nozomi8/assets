b2771be818a6d1a65d3e8eb41dab2dbc85461b7c
nozo38 <chno23014@g.nihon-u.ac.jp>
2024-10-10 20:04:07 +0900

    APPの追加

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header"
+import Navigarion from "./components/Navigation"
+import Main from "./components/Main"
+import Welcometext from "./components/Welcometext"
+import Imagesidebar from "./components/Imagesidebar"
+import Footer from "./components/Footer"
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Navigarion />
+      <Main />
+      <Welcometext />
+      <Imagesidebar />
+      <Footer />
+    </div>
+  );
+}
