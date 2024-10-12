6d1959bc382a714d5a9d83bb76aaeb38f5edf999
nozo38 <chno23014@g.nihon-u.ac.jp>
2024-10-12 11:15:51 +0900

    コンポーネントの中身の追加とhtmlの変更

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
+import Nav from "./Nav"
+
 export default function Header() {
   return (
-    <div>
-      <h1>Birdwatching</h1>
-      <img src="dove.png" alt="a simple dove logo"></img>
-    </div>
-  )
+    <header>
+      <div>
+        <h1>Birdwatching</h1>
+      </div>
+      <div className="img">
+        <img 
+          src="dove.png" 
+          alt="a simple dove logo"
+        />
+      </div>
+      <div>
+        <Nav />
+      </div>
+    </header>
+  );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import Welcomtext from "./Welcometext"
+import Imagesidebar from "./Imagesidebar"
+
+export default function Main() {
+  return (
+    <Main>
+      <div>
+        <Welcomtext />
+      </div> 
+      <div>
+        <Imagesidebar />
+      </div>
+    </Main>
+  );
+}
